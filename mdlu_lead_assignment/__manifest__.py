322fae2a5edd08e59f9772d223e3952aab04edde
cmeravi <cmeravi@modfuelwholesale.com>
2019-10-12 00:47:35 -0400

    [IMP] Adjusted Version

--- mdlu_lead_assignment/__manifest__.py
+++ mdlu_lead_assignment/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '1.0',
+    'version': '12.0.1.0',
     'images': ['static/description/banner.png',],
 
     # any module necessary for this one to work correctly
